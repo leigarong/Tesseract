2252e4732a976e2660c35c34e72d85391967efc4
leigarong <32857582+leigarong@users.noreply.github.com>
2017-10-17 10:30:29 +0800

    Embedded 3 DAQ
    Abelgas, Arnold Jay
    Bosa, Ralph Renz
    Garong, Leo Anthony
    Labios, El Rein
    Villamayor, Melisa Mae

---
+++ ES3/ES3.ino
@@ -0,0 +1,40 @@
+#include <dht.h>
+dht DHT;
+#define DHT11_PIN 7
+const int AOUTpin=0;
+const int DOUTpin=8;
+const int ledPin=13;
+
+int limit;
+int value;
+
+void setup(){
+Serial.begin(9600);
+pinMode(DOUTpin, INPUT);
+pinMode(ledPin, OUTPUT);
+}
+
+void loop()
+{
+  int chk = DHT.read11(DHT11_PIN);
+  
+  Serial.print(DHT.temperature);
+  Serial.print("|");
+  Serial.print(DHT.humidity);
+  
+  value= analogRead(AOUTpin);
+  limit= digitalRead(DOUTpin);
+ // Serial.print("CO value: ");
+   Serial.print("|");
+  Serial.println(value);
+  //Serial.print("Limit: ");
+  //Serial.print(limit);
+  delay(1000);
+  //if (limit == HIGH){
+  //digitalWrite(ledPin, HIGH); 
+ // }
+  //else{
+  //digitalWrite(ledPin, LOW);
+ // }
+}
+
